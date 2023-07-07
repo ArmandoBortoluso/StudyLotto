39e3ce4b2f61af8ab68c879067d2a8643674e73b
Armando B <armando.b.moura@gmail.com>
2023-07-07 09:12:07 -0300

    Ok first one.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿string filePath = @"./studyFolder/study.csv";
+
+Console.Write("Welcome to Study Lotto.\n");
+
+Console.WriteLine("Let's see what to study this week!");
+
+if(!File.Exists(filePath)){
+    Console.WriteLine("File not found. Make sure that the study.csv file exists in the directory studyFolder");
+
+} else {
+
+    List<String> fileLines = new List<string>();
+    Boolean isHeader = true;
+    string line;
+
+    using (FileStream fStream = File.OpenRead(filePath)) {
+
+        StreamReader? sR = new StreamReader(fStream);
+
+        while(!sR.EndOfStream){
+
+            line = sR.ReadLine();
+
+            //Ignore file header
+            if(isHeader){
+                isHeader = false;
+                continue;
+            }
+            
+            fileLines.Add(line);
+        }
+
+        sR.Close();
+    }
+
+    Random randNum = new Random();
+    
+    int luckyNum = randNum.Next(0, fileLines.Count);
+
+    string luckySub = fileLines[luckyNum];
+
+    Console.WriteLine("This week's subject is " + luckySub);
+
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # StudyLotto
+A program to simple generate a week subject to study for my drawing practice. Simple stuff.
+It reads a csv file in the studyFolder, that contains the subjects and draws one for this week
+practice.
